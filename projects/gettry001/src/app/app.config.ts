3e368bde3b0e0934ff70e6152e0cbb95c172b994
PRED-ONE\al5mo <al.5.montagni@gmail.com>
2025-02-07 17:35:18 +0100

    nws tabbar

--- projects/gettry001/src/app/app.config.ts
+++ projects/gettry001/src/app/app.config.ts
@@ -2,7 +2,18 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { RootNwsComponent } from './nwsmajor/root-nws/root-nws.component';
+import { RootOldComponent } from './oldminus/root-old/root-old.component';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter([
+      { path: 'root-nws', component: RootNwsComponent },
+      { path: 'root-old', component: RootOldComponent },
+      { path: '', redirectTo: '/root-nws', pathMatch: 'full' }
+    ])
+  ]
+
+   
 };
---
+++ projects/gettry001/src/app/oldminus/tabbar-old/tabbar-old.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabbar-old',
+  imports: [],
+  templateUrl: './tabbar-old.component.html',
+  styleUrl: './tabbar-old.component.css'
+})
+export class TabbarOldComponent {
+
+}
